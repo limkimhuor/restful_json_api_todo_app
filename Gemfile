f4c41cd47bc37897ad0ee659c984f94c25c267ca
limkimhuor <kimhuorlim@gmail.com>
2017-06-27 14:54:33 +0700

    add versioning serializer pagination

--- Gemfile
+++ Gemfile
@@ -61,3 +61,6 @@ end
 gem "dotenv-rails"
 gem "bcrypt", "~> 3.1.7"
 gem "jwt"
+gem "active_model_serializers", "~> 0.10.0"
+gem "will_paginate", "~> 3.1.0"
+gem "faker"
--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,10 +1,10 @@
-class TodosController < ApplicationController
+class V1::TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
   # GET /todos
   def index
     # get current user todos
-    @todos = current_user.todos
+    @todos = current_user.todos.paginate(page: params[:page], per_page: 20)
     json_response(@todos)
   end
 
